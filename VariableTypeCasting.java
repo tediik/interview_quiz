1d767605819248326cafa604fd1d3795081e292b
Eduard Nabiev <32924575+tediik@users.noreply.github.com>
2024-08-05 13:32:09 +0300

    Create VariableTypeCasting.java

---
+++ VariableTypeCasting.java
@@ -0,0 +1,8 @@
+// Будет ли работать этот код и, если нет, то почему?
+public class VariableTypeCasting { 
+    public static void main(String[] args) { 
+        byte a = 1;
+        byte b = 2;
+        byte c = a + b;
+    }
+}
